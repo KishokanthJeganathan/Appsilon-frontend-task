937422d42f0e42a308cc1048d4adbf55834df97c
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2021-01-18 21:40:39 +0100

    Update SalesMap.js

--- src/components/sales-map/SalesMap.js
+++ src/components/sales-map/SalesMap.js
@@ -7,28 +7,16 @@ import * as S from './styles';
 const geoUrl = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
 
 const markers = [
-	{
-		markerOffset: -15,
-		name: 'Buenos Aires',
-		coordinates: [ -58.3816, -34.6037 ]
-	},
 	{ markerOffset: -15, name: 'La Paz', coordinates: [ -68.1193, -16.4897 ] },
 	{ markerOffset: 25, name: 'Brasilia', coordinates: [ -47.8825, -15.7942 ] },
-	{ markerOffset: 25, name: 'Santiago', coordinates: [ -70.6693, -33.4489 ] },
 	{ markerOffset: 25, name: 'Bogota', coordinates: [ -74.0721, 4.711 ] },
-	{ markerOffset: 25, name: 'Quito', coordinates: [ -78.4678, -0.1807 ] },
-	{ markerOffset: -15, name: 'Georgetown', coordinates: [ -58.1551, 6.8013 ] },
-	{ markerOffset: -15, name: 'Asuncion', coordinates: [ -57.5759, -25.2637 ] },
-	{ markerOffset: 25, name: 'Paramaribo', coordinates: [ -55.2038, 5.852 ] },
-	{ markerOffset: 25, name: 'Montevideo', coordinates: [ -56.1645, -34.9011 ] },
-	{ markerOffset: -15, name: 'Caracas', coordinates: [ -66.9036, 10.4806 ] },
 	{ markerOffset: -15, name: 'Lima', coordinates: [ -77.0428, -12.0464 ] }
 ];
 
 const MapChart = () => {
 	return (
 		<S.Section>
-			<GraphTitle title="SUMMARY" />
+			<GraphTitle title="Highest performers in LATAM" />
 			<ComposableMap
 				style={{ width: '100%', height: '90%' }}
 				projection="geoAzimuthalEqualArea"
