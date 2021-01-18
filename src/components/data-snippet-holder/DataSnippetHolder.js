7ecc5cb7d779362522866f27442d50bd162ecb79
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2021-01-18 12:06:00 +0100

    Update DataSnippetHolder.js

--- src/components/data-snippet-holder/DataSnippetHolder.js
+++ src/components/data-snippet-holder/DataSnippetHolder.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { data } from '../../../utils/Data';
 import DataSnippet from '../data-snippet/DataSnippet';
 import * as S from './styles';
 
 const DataSnippetHolder = () => {
+	const data = [
+		{ sum: '$2 674 862', title: 'TOTAL PROFIT', change: 4.5, icon: 'BiCoinStack', iconBgColor: '#4FBFD7' },
+		{ sum: '657', title: 'ACTIVE USERS', change: 8.5, icon: 'BsPerson', iconBgColor: '#BDD74B' },
+		{ sum: '245', title: 'NEW ORDERS', change: 3.9, icon: 'BiGridVertical', iconBgColor: '#F49D42' },
+		{ sum: '12', title: 'OPEN COMPLAINTS', change: -5.3, icon: 'BsThreeDots', iconBgColor: '#ED4D4E' }
+	];
 	return (
 		<S.Section>
 			{data.map((snippet) => (
